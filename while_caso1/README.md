487ddde2aa48444dad6471c1fc200a9af8fd08a3
sistemas <germanordonez@colegioguanenta.edu.co>
2023-03-23 18:37:23 -0500

    se modifico while_caso1.py

---
+++ while_caso1/README.md
@@ -0,0 +1,3 @@
+ programa para saber los dies N primeros numeros
+# diagrama de flujo
+![Diagrama de fujo](diagrama.png "diagrma de flujo")
---
+++ while_caso1/while_caso1.py
@@ -0,0 +1,9 @@
+
+N = int(input("Digite el valor de un numero: "))
+suma = 0
+i = 1
+while (i<=N):
+    suma = suma+i
+    i = i+1
+print("suma es: ", suma)
+
